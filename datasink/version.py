af3764d13ba4669df0b9a749fcafcdb0146e1ec5
Eric Jeschke <eric@naoj.org>
2023-01-04 17:10:59 -1000

    Move to setuptools_scm, pyproject.toml and setup.cfg

--- datasink/version.py
+++
@@ -1,7 +0,0 @@
-# this file was automatically generated
-major = 2
-minor = 0
-release = 20201008233222
-
-version = '%d.%d.%d' % (major, minor, release)
-
--- setup.py
+++ setup.py
@@ -1,44 +1,7 @@
 #! /usr/bin/env python
 #
-from datasink.version import version
-import os
+# Licensed under a 3-clause BSD style license - see LICENSE.md
+#
+from setuptools import setup
 
-srcdir = os.path.dirname(__file__)
-
-try:
-    from setuptools import setup
-
-except ImportError:
-    from distutils.core import setup
-
-long_description = """
-datasink is data transfer software.
-"""
-
-setup(
-    name = "datasink",
-    version = version,
-    author = "Software Division, OCS Team",
-    author_email = "ocs@naoj.org",
-    description = ("Data Transfer Software for Subaru Telescope."),
-    long_description = long_description,
-    license = "BSD",
-    keywords = "data transfer subaru telescope",
-    url = "http://naojsoft.github.com/datasink",
-    packages = ['datasink'],
-    package_data = { },
-    scripts = ['scripts/datasink', # 'scripts/ds_client', 'scripts/ds_worker',
-               #'scripts/ds_sleep', 'scripts/ds_file',
-               ],
-    install_requires = ['pika>=1.1.0', 'pyyaml>=5.3.1'],
-    classifiers=[
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX',
-          'Programming Language :: Python :: 3.5',
-          'Topic :: Scientific/Engineering :: Astronomy',
-          'Topic :: Scientific/Engineering :: Physics',
-          ],
-)
+setup()
